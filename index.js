1de6b116528e8e49c14a270a10d02cfaa73e731b
victorb <hi.victorb@gmail.com>
2023-01-09 10:52:28 +0000

    setup express server

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
-const hello = "hello worldd"
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
 
-console.log(hello);
+dotenv.config();
+
+mongoose.connect(
+    process.env.MONGO_URL, 
+    { useNewUrlParser: true, useUnifiedTopology: true }, 
+    () => {
+        console.log("MongoDB Connected!");
+    }
+)
+
+// middlewares
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.get("/", (req, res) => {
+    res.json("api is working");
+})
+
+app.listen(8800, () => {
+    console.log("Backend server is running!");
+})
