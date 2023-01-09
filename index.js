5043e05ed98c8b2378d0378aa23592a1faa75067
victorb <hi.victorb@gmail.com>
2023-01-09 11:05:48 +0000

    added users and auth routes

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
+// routes
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
 
@@ -20,7 +23,8 @@ app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
 
-
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
 
 app.listen(8800, () => {
     console.log("Backend server is running!");
