e9e727023e53df39d556f4916291569025067ed8
victorb <hi.victorb@gmail.com>
2023-01-10 23:56:47 +0000

    login auth bug: fixed

--- routes/auth.js
+++ routes/auth.js
@@ -30,7 +30,7 @@ router.post("/register", async (req, res) => {
 router.post("/login", async (req, res) => {
     try {
         const user = await User.findOne({email: req.body.email});
-        !user && res.status(404).json("user not found");
+        if(!user) {res.status(404).json("user not found"); return; };
 
         const validPassword = await bcrypt.compare(req.body.password, user.password);
         if(!validPassword) {res.status(400).json("Wrong credentials!"); return;};
